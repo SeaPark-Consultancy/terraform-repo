cbe7258941379d4a37639a1b6a7c601a14030aa4
Michael-Seapark <michael.flynn@seaparkconsultancy.com>
2025-11-27 12:21:47 +0000

    Delete modules directory

--- modules/database/output.tf
+++
@@ -1,9 +0,0 @@
-output "database_id" {
-    value       = azurerm_mssql_database.example.id
-    description = "The ID of the SQL Database"
-}
-
-output "sql_server_name" {
-    value       = azurerm_mssql_server.example.name
-    description = "The name of the SQL Server"
-}
